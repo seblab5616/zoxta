d4f53cf0a6b98e600a6c5f64a122892c448b8d52
seblab5616 <konstantin.kosolapov@gmail.com>
2025-04-14 09:32:59 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
 
-// Firebase core
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYrJRPAtvlWqZyn13_O75ZEeW3Xii5Nw8",
-  authDomain: "zoxta-cf336.firebaseapp.com",
-  projectId: "zoxta-cf336",
-  storageBucket: "zoxta-cf336.appspot.com",
-  messagingSenderId: "170053701235",
-  appId: "1:170053701235:web:2b61745dd5e5ef5f226406"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { db };
